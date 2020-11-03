a979498863c6ad4677d993b489116c1b72b31a04
steff-dz <69957357+steff-dz@users.noreply.github.com>
2020-11-03 16:11:55 +0100

    setting up pages

---
+++ scripts/nav.js
@@ -0,0 +1,7 @@
+const nav = document.createElement('nav');
+const siteTitle = document.createElement('h1');
+nav.id = 'nav';
+siteTitle.id = 'siteTitle';
+document.body.appendChild(nav);
+nav.appendChild(siteTitle);
+siteTitle.innerHTML = `SARAH BOWES DESIGN.`;
