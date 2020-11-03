1468e31fedc9ca9c3d5a8961e09df3c5f077cdd1
steff-dz <69957357+steff-dz@users.noreply.github.com>
2020-11-03 22:31:35 +0100

    finished writing the form, starting the main func

--- scripts/custom-order.js
+++ scripts/custom-order.js
@@ -1,2 +1,35 @@
-console.log(document.body.children);
 document.body.insertBefore(nav, document.body.children[0]);
+
+const main = document.querySelector('main');
+const form = document.querySelector('form');
+const sections = document.querySelectorAll('section');
+const hammers = document.querySelectorAll('.fa-hammer');
+const buttons = document.querySelectorAll('button');
+
+console.log(buttons[0]);
+
+buttons[0].addEventListener('click', function() {
+	dropHammer(enterForm);
+});
+
+function enterForm() {
+	main.style.display = 'none';
+	createProgressBar();
+	sections[0].style.visibility = 'visible';
+	form.style.left = '10vw';
+	sections[0].style.transform = 'translateX(-10px)';
+	console.log(sections[0]);
+}
+
+function dropHammer(cb) {
+	console.log('The hammer has dropped');
+	hammers[0].classList.add('hammerDown');
+	return setTimeout(cb, 800);
+}
+
+function createProgressBar() {
+	let progressContainer = document.createElement('div');
+	progressContainer.innerHTML = 'HOLLAAAA AMIIIGOS';
+	progressContainer.id = 'progressContainer';
+	document.body.appendChild(progressContainer);
+}
