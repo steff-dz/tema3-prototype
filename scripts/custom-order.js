84682c3f35445ed3cddb4e2e671724c0ed9622d1
steff-dz <69957357+steff-dz@users.noreply.github.com>
2020-11-04 18:53:05 +0100

    added progressbar

--- scripts/custom-order.js
+++ scripts/custom-order.js
@@ -6,8 +6,6 @@ const sections = document.querySelectorAll('section');
 const hammers = document.querySelectorAll('.fa-hammer');
 const buttons = document.querySelectorAll('button');
 
-console.log(buttons[0]);
-
 buttons[0].addEventListener('click', function() {
 	dropHammer(enterForm);
 });
@@ -15,21 +13,24 @@ buttons[0].addEventListener('click', function() {
 function enterForm() {
 	main.style.display = 'none';
 	createProgressBar();
+	progressContainer.children[0].style.backgroundColor = 'lightgreen';
 	sections[0].style.visibility = 'visible';
 	form.style.left = '10vw';
 	sections[0].style.transform = 'translateX(-10px)';
-	console.log(sections[0]);
 }
 
 function dropHammer(cb) {
-	console.log('The hammer has dropped');
 	hammers[0].classList.add('hammerDown');
 	return setTimeout(cb, 800);
 }
 
 function createProgressBar() {
 	let progressContainer = document.createElement('div');
-	progressContainer.innerHTML = 'HOLLAAAA AMIIIGOS';
 	progressContainer.id = 'progressContainer';
 	document.body.appendChild(progressContainer);
+	progressContainer.innerHTML = `
+	<div class="progressBox">1</div>
+	<div class="progressBox">2</div>
+	<div class="progressBox">3</div>
+	`;
 }
